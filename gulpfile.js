d004df68aecc8cbc33b3bdd9fab66ac6d4b03adb
Diana Murtazina <murka8831@yandex.ru>
2023-01-16 00:03:09 +0400

    added markup, styles

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
 const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
 
-const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const dev = gulp.series(reset, svgSprive, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks);
 const deployZIP = gulp.series(reset, mainTasks, zip);
 const deployFTP = gulp.series(reset, mainTasks, ftp);
