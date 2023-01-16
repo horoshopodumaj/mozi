0983efd0f0d8e1b40fd4525b739eba9b5e88e16c
Diana Murtazina <murka8831@yandex.ru>
2023-01-16 19:47:25 +0400

    added burger-menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,5 @@
 import * as flsFunctions from "./modules/functions.js";
+import * as burgerMenu from "./modules/burger-menu.js";
 
 flsFunctions.isWebp();
-
-import Swiper, { Navigation, Pagination } from "swiper";
-
-const swiper = new Swiper();
+burgerMenu.burgerMenu();
---
+++ src/js/modules/burger-menu.js
@@ -0,0 +1,31 @@
+export function burgerMenu() {
+    const menu = document.getElementById("burger-menu");
+    const menuClose = document.getElementById("burger-menu-close");
+    const menuOpen = document.getElementById("burger-menu-open");
+    const menuBody = document.querySelector(".burger-menu__menu");
+
+    if (menuOpen) {
+        menuOpen.addEventListener("click", function () {
+            if (menu) {
+                menu.setAttribute("data-open", "");
+                document.body.classList.add("_lock");
+            }
+        });
+    }
+
+    if (menuClose) {
+        menuClose.addEventListener("click", function () {
+            if (menu) {
+                menu.removeAttribute("data-open");
+                document.body.classList.toggle("_lock");
+            }
+        });
+    }
+
+    if (menuBody) {
+        menuBody.addEventListener("click", function () {
+            menu.removeAttribute("data-open");
+            document.body.classList.toggle("_lock");
+        });
+    }
+}
